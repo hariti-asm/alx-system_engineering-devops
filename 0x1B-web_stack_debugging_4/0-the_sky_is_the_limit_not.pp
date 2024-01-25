427dae0d02a0ac661bf22f6599a66e21e3981c53
hariti-asm <haritiasmaa74.com>
2024-01-25 14:56:36 -0800

    ...

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,15 +1,14 @@
-# Increases the amount of traffic an Nginx server can handle
+# This manuscript increases the amount of traffic an Nginx server can handle
 
 # Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin'
+file { 'fix-for-nginx':
+  ensure  => 'file',
+  path    => '/etc/default/nginx',
+  content => inline_template('<%= File.read("/etc/default/nginx").gsub(/15/, "4096") %>'),
 }
 
-#restart Nginx
-
-exec { 'nginx-restart':
+# Restart Nginx
+-> exec { 'nginx-restart':
   command => 'nginx restart',
-  path    => '/etc/init.d/'
+  path    => '/etc/init.d/',
 }
-
